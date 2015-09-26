9f3a786e00ad6cbe6c05cbbbd7fb64b5be252f13
Stas Vilchik <vilchiks@gmail.com>
2015-09-26 12:57:08 +0200

    activate nvm

--- travis.sh
+++ travis.sh
@@ -36,7 +36,7 @@ MYSQL)
 WEB)
   installTravisTools
 
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+  . ~/.nvm/nvm.sh
   nvm install 4.1.1
   cd server/sonar-web && npm install
   ;;
